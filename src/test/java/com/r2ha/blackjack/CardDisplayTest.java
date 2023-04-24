3446019bb7dc8322faa5c94e963bbcb874940e1e
Sophia Brandt <git@s-br.net>
2023-04-24 17:50:53 +0200

    test(lab1): add display tests for rank 10

---
+++ src/test/java/com/r2ha/blackjack/CardDisplayTest.java
@@ -0,0 +1,29 @@
+package com.r2ha.blackjack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CardDisplayTest {
+
+    private static final Suit DUMMY_SUIT = Suit.HEARTS;
+    private static final Rank DUMMY_RANK = Rank.TEN;
+
+    @Test
+    public void displayTenAsString() throws Exception {
+        Card card = new Card(DUMMY_SUIT, DUMMY_RANK);
+
+        assertThat(card.display()).isEqualTo(
+                "[31m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘"
+        );
+    }
+
+    @Test
+    public void displayNonTenAsString() throws Exception {
+        Card card = new Card(DUMMY_SUIT, Rank.SEVEN);
+
+        assertThat(card.display()).isEqualTo(
+                "[31m┌─────────┐[1B[11D│7        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        7│[1B[11D└─────────┘"
+        );
+    }
+}
