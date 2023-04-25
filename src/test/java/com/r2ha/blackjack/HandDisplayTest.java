54e2822e3efd55925cd607ddeb86b30eb5107adf
Sophia Brandt <git@s-br.net>
2023-04-25 16:08:20 +0200

    test(lab2): add test for cardsAsString

--- src/test/java/com/r2ha/blackjack/HandDisplayTest.java
+++ src/test/java/com/r2ha/blackjack/HandDisplayTest.java
@@ -20,4 +20,17 @@ class HandDisplayTest {
                         "[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
     }
 
+    @Test
+    void cardsAsStringTranslatesTostring() throws Exception {
+        List<Card> cards = List.of(
+                new Card(Suit.HEARTS, Rank.ACE),
+                new Card(Suit.CLUBS, Rank.JACK));
+        Hand hand = new Hand(cards);
+
+        assertThat(hand.cardsAsString())
+                .isEqualTo(
+                        "[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘[6A[1C[30m┌─────────┐[1B[11D│J        │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│        J│[1B[11D└─────────┘"
+                );
+
+    }
 }
